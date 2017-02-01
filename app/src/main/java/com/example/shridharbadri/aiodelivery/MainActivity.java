f2572ec61c6c525bd50efd3247a7dc322f237c46
Srikant <srikantbadri96@gmail.com>
2017-02-01 13:13:19 +0530

    Initial Commit

--- app/src/main/java/com/example/shridharbadri/aiodelivery/MainActivity.java
+++ app/src/main/java/com/example/shridharbadri/aiodelivery/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.shridharbadri.aiodelivery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Intent it=new Intent(MainActivity.this,LoginActivity.class);
+        Thread td=new Thread(new Runnable() {
+            @Override
+            public void run() {
+//                 td.sleep();
+                try {
+                    sleep(3000);
+                    startActivity(it);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        td.start();
     }
 }
