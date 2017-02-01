2b66a56f3e25c8d121b43ac3abd513c575b69c55
Srikant <srikantbadri96@gmail.com>
2017-02-01 14:28:32 +0530

    Splash Screen Image Added

--- app/src/main/java/com/example/shridharbadri/aiodelivery/MainActivity.java
+++ app/src/main/java/com/example/shridharbadri/aiodelivery/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.shridharbadri.aiodelivery;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import static java.lang.Thread.sleep;
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         final Intent it=new Intent(MainActivity.this,LoginActivity.class);
         Thread td=new Thread(new Runnable() {
             @Override
