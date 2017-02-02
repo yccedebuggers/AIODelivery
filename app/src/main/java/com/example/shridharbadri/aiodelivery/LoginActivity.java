30107ec0e0898586e12e45970b2c372df0c61157
yccedebuggers <debuggers007@gmail.com>
2017-02-02 20:44:46 +0530

    Adding Log In Button And Foreget Password Text

--- app/src/main/java/com/example/shridharbadri/aiodelivery/LoginActivity.java
+++ app/src/main/java/com/example/shridharbadri/aiodelivery/LoginActivity.java
@@ -2,12 +2,39 @@ package com.example.shridharbadri.aiodelivery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView ForgetText;
+    Button btnLogIn,btnCancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ForgetText=(TextView)findViewById(R.id.textView3);
+        btnLogIn=(Button)findViewById(R.id.button2);
+        btnCancel=(Button)findViewById(R.id.button);
+        ForgetText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //ForgetText.setClickable(false);
+            }
+        });
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnLogIn.setEnabled(false);
+                btnCancel.setEnabled(true);
+
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
