cc2f148b372379d1c9b3136f17b395cdac8e71f9
yccedebuggers <debuggers007@gmail.com>
2017-02-03 06:07:39 +0530

    Added Animation to Button

--- app/src/main/java/com/example/shridharbadri/aiodelivery/LoginActivity.java
+++ app/src/main/java/com/example/shridharbadri/aiodelivery/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.shridharbadri.aiodelivery;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -25,6 +27,12 @@ public class LoginActivity extends AppCompatActivity {
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Animation bounce= AnimationUtils.loadAnimation(getApplication(),R.anim.bounce);
+                MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+                bounce.setInterpolator(interpolator);
+
+                btnLogIn.startAnimation(bounce);
+
                 btnLogIn.setEnabled(false);
                 btnCancel.setEnabled(true);
 
@@ -36,5 +44,19 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+}
+class MyBounceInterpolator implements android.view.animation.Interpolator {
+    double mAmplitude = 1;
+    double mFrequency = 10;
+
+    MyBounceInterpolator(double amplitude, double frequency) {
+        mAmplitude = amplitude;
+        mFrequency = frequency;
+    }
+
+    public float getInterpolation(float time) {
+        return (float) (-1 * Math.pow(Math.E, -time/ mAmplitude) *
+                Math.cos(mFrequency * time) + 1);
     }
 }
