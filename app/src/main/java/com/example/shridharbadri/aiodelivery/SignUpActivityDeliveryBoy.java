70d022236c634859dee711a62a689fe355568f02
Srikant <srikantbadri96@gmail.com>
2017-02-03 19:26:28 +0530

    Added SignUp Activity For Delivery Boy.

---
+++ app/src/main/java/com/example/shridharbadri/aiodelivery/SignUpActivityDeliveryBoy.java
@@ -0,0 +1,13 @@
+package com.example.shridharbadri.aiodelivery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivityDeliveryBoy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_delivery_boy);
+    }
+}
