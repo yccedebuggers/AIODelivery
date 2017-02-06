81fd02d5dc58819465de72c228c788605cddba80
yccedebuggers <debuggers007@gmail.com>
2017-02-06 21:21:04 +0530

    field for email is added

--- app/src/main/java/com/example/shridharbadri/aiodelivery/SignUpActivityOwner.java
+++ app/src/main/java/com/example/shridharbadri/aiodelivery/SignUpActivityOwner.java
@@ -6,13 +6,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivityOwner extends Fragment {
-
+    EditText edtName,edtuserName,edtEmail,edtPass,edtRePass,edtAdd1,edtAdd2,edtAdd3;
+    Button btnCancel,btnSave;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.activity_sign_up_owner, container, false);
 
         return rootView;
